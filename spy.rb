dcf12f9360fced76f172d222cd78605c43eb346a
Stefan Saasen <stefan@saasen.me>
2013-12-18 09:41:36 +1100

    Rev spy to version 0.12

--- spy.rb
+++ spy.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Spy < Formula
   homepage 'https://bitbucket.org/ssaasen/spy'
-  url 'https://bitbucket.org/ssaasen/spy/downloads/spy-osx-x86_64-v0.11.tar.gz'
-  sha1 '211849bfa2d711e285927b7590dd4577e9e4aad0'
-  version '0.11'
+  url 'https://bitbucket.org/ssaasen/spy/downloads/spy-osx-x86_64-v0.12.tar.gz'
+  sha1 '978d321f2f9956dc836b75bd4d552205798e3cf0'
+  version '0.12'
 
   def install
     bin.install Dir['bin/*']
