cf437c4c0b181976f25ca706b36f50ab535d2e41
Stefan Saasen <ssaasen@atlassian.com>
2014-02-19 18:38:46 +1100

    Fix Formular name from Spy -> StashCommitGraphReader

--- stash-commit-graph-reader.rb
+++ stash-commit-graph-reader.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Spy < Formula
+class StashCommitGraphReader < Formula
   homepage 'https://bitbucket.org/ssaasen/stash-commit-graph-reader'
   url 'https://bitbucket.org/ssaasen/stash-commit-graph-reader/downloads/stash-commit-graph-reader-0.1.tar.gz'
   sha1 '20e8e8639797ee0a0ffd3d2fe02a7210d2688239'
