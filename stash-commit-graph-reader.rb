a95b1e07e30744f18821aa576a1faf9b3b79821f
Stefan Saasen <ssaasen@atlassian.com>
2014-02-21 20:53:31 +1100

    Rev stash-commit-graph-reader from 0.2 to 0.3

--- stash-commit-graph-reader.rb
+++ stash-commit-graph-reader.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class StashCommitGraphReader < Formula
   homepage 'https://bitbucket.org/ssaasen/stash-commit-graph-reader'
-  url 'https://bitbucket.org/ssaasen/stash-commit-graph-reader/downloads/stash-commit-graph-reader-0.2.tar.gz'
-  sha1 'c527b3747fad33eda83737fa307e43c7ca99f225'
-  version '0.2'
+  url 'https://bitbucket.org/ssaasen/stash-commit-graph-reader/downloads/stash-commit-graph-reader-0.3.tar.gz'
+  sha1 'cae1fec162730f20cbb75d7c6ec8e8b940b72d86'
+  version '0.3'
 
   def install
     bin.install Dir['bin/*']
