80f40fc67ed363f6bede31265aa3e45d99bcefcc
Alan Berman <thealanberman@gmail.com>
2016-09-12 16:19:28 -0700

    updated to use sha256 hash value

--- stash-logparser.rb
+++ stash-logparser.rb
@@ -3,7 +3,7 @@ require 'formula'
 class StashLogparser < Formula
   homepage 'https://bitbucket.org/ssaasen/stash-log-parser'
   url 'https://bitbucket.org/ssaasen/stash-log-parser/downloads/logparser-osx-x86_64-v3.0.tar.gz'
-  sha1 '7ffde894eedf0c0a2aecf21ab20e9a0478d2ecd7'
+  sha256 'ffa942001c4fa3d8d237e5f0976ecd1aa944755c7bde9fb3d8c04e7e3d2476ed'
   version '3.0'
 
   def install
