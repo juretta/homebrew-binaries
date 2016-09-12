bd3dcd5c980fd2606497dfc8c602c97c8958f206
Alan Berman <thealanberman@gmail.com>
2016-09-12 16:18:30 -0700

    updated to sha256 hash value

--- stash-commit-graph-reader.rb
+++ stash-commit-graph-reader.rb
@@ -3,7 +3,7 @@ require 'formula'
 class StashCommitGraphReader < Formula
   homepage 'https://bitbucket.org/ssaasen/stash-commit-graph-reader'
   url 'https://bitbucket.org/ssaasen/stash-commit-graph-reader/downloads/stash-commit-graph-reader-0.3.tar.gz'
-  sha1 'cae1fec162730f20cbb75d7c6ec8e8b940b72d86'
+  sha256 '5fa62bcda10b76e676899fc82901302b273451c91e7f8a883776e6779faf01a8'
   version '0.3'
 
   def install
