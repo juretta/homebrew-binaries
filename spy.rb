c503f91e1fc0463ed70437cf7f3a609b28640d25
Stefan Saasen <ssaasen@atlassian.com>
2016-09-16 18:13:31 +1000

    Bump spy from 0.12 to 0.13

--- spy.rb
+++ spy.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Spy < Formula
   homepage 'https://bitbucket.org/ssaasen/spy'
-  url 'https://bitbucket.org/ssaasen/spy/downloads/spy-osx-x86_64-v0.12.tar.gz'
-  sha256 '84197708bd771f87d8b792b3c0f2178852065f101b5fc4195dcfcec88f1676b4'
-  version '0.12'
+  url 'https://bitbucket.org/ssaasen/spy/downloads/spy-osx-x86_64-v0.13.tar.gz'
+  sha256 '077fafd355159d0172bfdfe98e4a692b19daeaedc44f395026e666a35236b226'
+  version '0.13'
 
   def install
     bin.install Dir['bin/*']
