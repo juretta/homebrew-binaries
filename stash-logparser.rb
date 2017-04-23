134ebaf1a55937f540ed044a64bec53cee0a8584
Stefan Saasen <ssaasen@atlassian.com>
2017-04-23 18:53:38 +0200

    Bump stash-logparser from 3.0 to 3.1

--- stash-logparser.rb
+++ stash-logparser.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class StashLogparser < Formula
   homepage 'https://bitbucket.org/ssaasen/stash-log-parser'
-  url 'https://bitbucket.org/ssaasen/stash-log-parser/downloads/logparser-osx-x86_64-v3.0.tar.gz'
-  sha256 'ffa942001c4fa3d8d237e5f0976ecd1aa944755c7bde9fb3d8c04e7e3d2476ed'
-  version '3.0'
+  url 'https://bitbucket.org/ssaasen/stash-log-parser/downloads/logparser-osx-x86_64-v3.1.tar.gz'
+  sha256 '1c0a59ca5b72c25e0c44e5bd79b7695b5bbf1d19e89d9fc44d8e57e5af752b5d'
+  version '3.1'
 
   def install
     bin.install Dir['bin/*']
